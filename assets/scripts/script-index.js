ed2ca02ca22c8caea84e098d1edc144a48d1a4d4
Sukhjinder Arora <sukhb1995@gmail.com>
2018-07-26 23:34:53 +0530

    replaced jquery ajax with fetch api

--- assets/scripts/script-index.js
+++ assets/scripts/script-index.js
@@ -1,60 +1,53 @@
 "use strict";
 
-$(document).ready(function () {
+const menuButton = document.getElementById("menuBtn");
+const closeButton = document.getElementById("closeBtn");
+const sideNav = document.getElementById("sideNav");
+const afterSubmit = document.getElementById("submitSuccess");
+const navBarLinks = document.getElementsByClassName("navBar__link");
 
-    let menuButton = document.getElementById("menuBtn");
-    let closeButton = document.getElementById("closeBtn");
-    let sideNav = document.getElementById("sideNav");
-    let afterSubmit = document.getElementById("submitSuccess");
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 550,
+  easing: "easeInOutQuad"
+});
 
-    // Toggle Side Menu
+// Toggle Side Menu
 
-    function toggleSideMenu() {
-        sideNav.classList.toggle("isActive");
+function toggleSideMenu() {
+  sideNav.classList.toggle("isActive");
+}
+
+menuButton.addEventListener("click", toggleSideMenu);
+closeButton.addEventListener("click", toggleSideMenu);
+
+Array.from(navBarLinks).forEach(link =>
+  link.addEventListener("click", toggleSideMenu)
+);
+
+// Form Submission using AJAX
+
+const form = document.getElementById("ajaxForm");
+const url = form.getAttribute("action");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  fetch(url, {
+    method: "POST",
+    mode: "no-cors",
+    body: new URLSearchParams(new FormData(form)).toString(),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
     }
-
-    menuButton.addEventListener("click", toggleSideMenu);
-    closeButton.addEventListener("click", toggleSideMenu);
-
-    // Smooth Scroll when navigating by clicking on links
-
-    $(".navBar__link, .profile__link, .content__link--contact").click(
-      function(e) {
-        var curLink = $(this);
-        var scrollPoint = $(curLink.attr("href")).position().top;
-        $("body,html").animate({ scrollTop: scrollPoint }, 700);
-          if (curLink.hasClass("navBar__link")) {
-            toggleSideMenu();
-          }
-            
-      }
-    );
-
-    // Form Submission using AJAX
-
-    $(function () {
-        $(".js-contactForm").submit(function (e) {
-            e.preventDefault();
-            var href = $(this).attr("action");
-            $.ajax({
-                type: "POST",
-                dataType: "json",
-                url: href,
-                data: $(this).serialize(),
-                success: function (response) {
-                    if (response.status == "success") {
-                        afterSubmit.style.opacity = "1";
-                        $(".js-contactForm").trigger("reset");
-                        window.setTimeout(function () {
-                            afterSubmit.style.opacity = "0";
-                        }, 6000);
-
-                    } else {
-                        alert("An error occured: " + response.message);
-                    }
-                }
-            });
-        });
+  })
+    .then(response => {
+      form.reset();
+      afterSubmit.style.opacity = "1";
+      window.setTimeout(function() {
+        afterSubmit.style.opacity = "0";
+      }, 4000);
+    })
+    .catch(error => {
+      alert("Something went wrong!!");
     });
-
 });
