0ae5d80cf2ee7449a4a253aa479e1a0c40781422
Sukhjinder Arora <social.sukhjinderarora@gmail.com>
2020-02-01 22:20:34 +0530

    Added Google Recaptcha

--- assets/scripts/script-index.js
+++ assets/scripts/script-index.js
@@ -6,6 +6,14 @@ const sideNav = document.getElementById("sideNav");
 const afterSubmit = document.getElementById("submitSuccess");
 const navBarLinks = document.querySelectorAll(".navBar__link");
 const form = document.getElementById("ajaxForm");
+const successMessage = document.getElementById("successMsg");
+
+const gRecaptchaWidget = document.getElementById("g-recaptcha");
+
+let gRecaptchaWidgetID;
+
+const RC2KEY = '6Lcy2dQUAAAAAP5xbr19Br7Mj0ufp6XgIQSeQeAo';
+let doSubmit = false;
 
 const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 550,
@@ -34,24 +42,61 @@ form.addEventListener("submit", function(event) {
   event.preventDefault();
   const url = this.action;
 
-  fetch(url, {
-    method: "POST",
-    mode: "no-cors",
-    body: new URLSearchParams(new FormData(form)).toString(),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
-    }
-  })
-    .then(response => {
-      form.reset();
-      afterSubmit.style.opacity = "1";
-      window.setTimeout(function() {
-        afterSubmit.style.opacity = "0";
-      }, 4000);
+  if (doSubmit) {
+    fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      body: new URLSearchParams(new FormData(form)).toString(),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+      }
     })
-    .catch(error => {
-      alert("Something went wrong!!");
-    });
+      .then(response => {
+        form.reset(gRecaptchaWidgetID);
+        successMessage.textContent = "Message Sent!";
+        afterSubmit.style.opacity = "1";
+        window.setTimeout(function () {
+          afterSubmit.style.opacity = "0";
+        }, 4000);
+        grecaptcha.reset(gRecaptchaWidgetID);
+      })
+      .catch(error => {
+        alert("Something went wrong!!");
+      });
+  } else {
+    successMessage.textContent = "Please complete the captcha!";
+    afterSubmit.style.opacity = "1";
+    window.setTimeout(function () {
+      afterSubmit.style.opacity = "0";
+    }, 4000);
+
+  }
 
 });
+
+// Google Recaptcha 
+
+function reCaptchaVerify(response) {
+  if (response === grecaptcha.getResponse()) {
+    doSubmit = true;
+  }
+}
+
+function reCaptchaExpired() {
+  /* do something when it expires */
+  doSubmit = false;
+}
+
+
+function reCaptchaCallback() {
+  /* this must be in the global scope for google to get access */
+  gRecaptchaWidgetID = grecaptcha.render(gRecaptchaWidget, {
+    'sitekey': RC2KEY,
+    'callback': reCaptchaVerify,
+    'expired-callback': reCaptchaExpired,
+    'theme': 'dark',
+  });
+}
+
+
